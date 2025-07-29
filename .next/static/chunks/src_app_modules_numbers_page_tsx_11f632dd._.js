0e6a2c5ba944b4f62ad134873a4c84ab21d7c70f
Gemmahadi <142386744+Gemmahadi@users.noreply.github.com>
2025-07-29 12:09:24 +0700

    Add .next/static/chunks/src_app_modules_numbers_page_tsx_11f632dd._.js 1753765762524

---
+++ .next/static/chunks/src_app_modules_numbers_page_tsx_11f632dd._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_modules_numbers_page_tsx_76224b5a._.js"
+],
+    source: "dynamic"
+});
